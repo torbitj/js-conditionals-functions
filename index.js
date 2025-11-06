5af3ade1c9f98742778180b8dc368cb729d70362
JT <jstorbit@gmail.com>
2025-11-05 17:49:11 -0700

    Create user prompt function

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const isOdd = (number) => {
   }
   return `That's an even number!`
 }
+
+const userInput = () => {
+  const numberStr = prompt(`Enter a number`);
+  const isNumber = isNaN(numberStr);
+
+  if (!isNumber) {
+    const number = Number(numberStr);
+    const message = isOdd(number);
+    return message
+  }
+}
+
+console.log(userInput())
