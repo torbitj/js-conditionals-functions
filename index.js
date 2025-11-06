bb60748586d94d338e3fbc8d40b2a554d5df2783
JT <jstorbit@gmail.com>
2025-11-05 18:34:32 -0700

    Add alert if user inputs anything but a number

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const isOdd = (number) => {
-  if (number % 2 === 1) {
+  if (number % 2 === 1 || number % 2 === -1) {
     return `That's an odd number!`
   }
-  return `That's an even number!`
+  return `That's an even better number!`
 }
 
 const userInput = () => {
@@ -14,6 +14,8 @@ const userInput = () => {
     const message = isOdd(number);
     return message
   }
+  alert(`That is not a number, please try again!`)
+  userInput();
 }
 
 alert(userInput());
